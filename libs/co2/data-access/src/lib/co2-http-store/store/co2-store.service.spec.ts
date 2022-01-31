7b2809afac5705b2f946a2f4f1c407392822078e
TopGitSan <c.toparcean@gmail.com>
2022-01-31 17:10:04 +0100

    feat: add records$ test

--- libs/co2/data-access/src/lib/co2-http-store/store/co2-store.service.spec.ts
+++ libs/co2/data-access/src/lib/co2-http-store/store/co2-store.service.spec.ts
@@ -1,11 +1,12 @@
 import { Co2Store } from "./co2-store.service";
 import { TestBed } from "@angular/core/testing";
+import { firstValueFrom } from "rxjs";
 
 describe(Co2Store.name, () => {
 
   function setup() {
     TestBed.configureTestingModule({
-      providers:[Co2Store]
+      providers: [Co2Store]
     })
     const store = TestBed.inject(Co2Store);
 
@@ -19,5 +20,14 @@ describe(Co2Store.name, () => {
     const { store } = setup();
 
     expect(store).not.toBeNull();
+  })
+
+  describe('records$', () => {
+    it('initialy emits records', async () => {
+      const { store } = setup();
+      const result = await firstValueFrom(store.records$)
+
+      expect(result).toEqual([]);
+    })
   })
 })
--- libs/co2/data-access/src/lib/co2-http-store/store/co2-store.service.ts
+++ libs/co2/data-access/src/lib/co2-http-store/store/co2-store.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ComponentStore } from "@ngrx/component-store";
-import { CO2EmissionsRecord } from "../http/co2-record.interface";
+import { Observable } from "rxjs";
+import { CO2EmissionsRecord, CO2EmissionsRecords } from "../http/co2-record.interface";
 
 export interface Co2State {
-  readonly records:  readonly CO2EmissionsRecord[]
+  readonly records: readonly CO2EmissionsRecord[]
 }
 
 @Injectable()
 export class Co2Store extends ComponentStore<Co2State>{
-  constructor(){
+  records$: Observable<CO2EmissionsRecords> = this.select(state => state.records);
+
+  constructor() {
     super(initialState);
 
     // or
@@ -16,6 +19,7 @@ export class Co2Store extends ComponentStore<Co2State>{
   }
 }
 
-const initialState:Co2State = {
+
+const initialState: Co2State = {
   records: []
 }
