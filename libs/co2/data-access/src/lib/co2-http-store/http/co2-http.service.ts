c38ee699bbdf9a78f098445184c85811d70fe1f5
TopGitSan <c.toparcean@gmail.com>
2022-02-02 14:48:30 +0100

    feat: implement most of HTTP client

--- libs/co2/data-access/src/lib/co2-http-store/http/co2-http.service.ts
+++ libs/co2/data-access/src/lib/co2-http-store/http/co2-http.service.ts
@@ -1,18 +1,35 @@
-import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
-import { CO2EmissionsRecord } from "./co2-record.interface";
-
-const url = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql';
-
-// sql param
-// SELECT "Minutes5UTC", "MinutesSDK", "PriceArea", "CO2Emissions"
-// FROM "co2emisprog" ORDERBY "Minutes5UTC" DESC LIMIT 100;
+import { Injectable } from '@angular/core';
+import { mergeMap, Observable, of, throwError } from 'rxjs';
+import { CO2EmissionsRecord } from './co2-record.interface';
+import { HttpClient } from '@angular/common/http';
+import { CkanResponseInterface } from './ckan-response.interface';
+import { CkanErrorResponseInterface } from './ckan-error-response.interface';
+import { nrgDataServiceEndpoint } from './nrg-data-service-endpoint';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Co2Http {
+  constructor(private http: HttpClient) {}
   get(): Observable<readonly CO2EmissionsRecord[]> {
-    return of([]);
+    // todo : query only 2 days
+    const sql = `SELECT
+    "Minutes5UTC" AS "minutes5UTC",
+    "PriceArea" AS "priceArea",
+    "CO2Emissions" AS "co2Emissions"
+    FROM "co2emisprog"
+    -- TODO WHERE Clause
+    ORDERBY "Minutes5UTC" ASC LIMIT 100;`;
+    return this.http
+      .get<
+        CkanResponseInterface<CO2EmissionsRecord> | CkanErrorResponseInterface
+      >(nrgDataServiceEndpoint, { params: { sql } })
+      .pipe(
+        mergeMap(response =>
+          response.success
+            ? of(response.result.records.map(v=>({...v,minutes5UTC: new Date(v.minutes5UTC)})))
+            : throwError(() => new Error('CKAN Error'))
+        )
+      );
   }
 }
---
+++ libs/co2/data-access/src/lib/co2-http-store/http/nrg-data-service-endpoint.ts
@@ -0,0 +1,2 @@
+
+export const nrgDataServiceEndpoint = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql';
